02db59654e3a75c44dac9723f712321770117d61
Willard Kang <weig.kang@gmail.com>
2020-11-20 23:47:17 +0800

    Day2. Upload files and save in database

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 
 ### Day1 管理資料夾和檔案
 第一天, 今天參考 IAmTimCorey 的教學影片, 學習如何對檔案進行存取。主要使用到 System.IO 的 Directory 類別。
+
+### Day2. 上傳檔案並將檔案存至 SQL Server
+ 
+
+##### 今天參考的是這篇 [How to Upload Files and Save in Database in ASP.NET Core MVC](https://tutexchange.com/how-to-upload-files-and-save-in-database-in-asp-net-core-mvc/), 文章寫得非常詳細, 只要按照他的步驟就能夠成功將檔案存入資料庫中。
---
+++ UploadAndSave/Models/DatabaseContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using UploadAndSave.Models;
+
+namespace UploadAndSave.Models
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Files> Files { get; set; }
+    }
+}
