1423ec7ff801eb0fa197202552b976c1fd628c46
Willard Kang <weig.kang@gmail.com>
2020-12-30 22:23:41 +0800

    Update Pascal's Triangle

---
+++ LeetCode/Pascal's Triangle/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Pascal_s_Triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
